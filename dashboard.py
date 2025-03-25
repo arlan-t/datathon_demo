f7749ca297910d1eaabaed1dff3eb5801f7677e1
arlan-t <arlantoktarkhan1@gmail.com>
2025-03-26 00:40:43 +0500

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -727,7 +727,7 @@ if __name__ == "__main__":
     map_df_priority = map_df_priority.dropna(subset=['Investment_Priority_Index'])
 
     with col3:
-        animated_fig = create_animated_map(map_df, geojson_data, featureidkey, "Depreciation rate")
+        animated_fig = create_animated_map(map_df, geojson_data, featureidkey, "Depreciation_Rate")
         st.plotly_chart(animated_fig, use_container_width=True)
 
     with col4:
